554e8b67a2fac892e41f2d780c5b5289de8f0bc9
Salavat <salavat2602@mail.ru>
2025-11-14 13:42:03 +0500

    final

--- src/index.js
+++ src/index.js
@@ -1,32 +1,39 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createRoot } from 'react-dom/client';
+import {createStore} from 'redux'
 
 const initialState = {value: 0};
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case "INC":
+        case  "INC" :
             return {
                 ...state,
-                value: state.value + 1
+                value: state.value + 1 
             };
-        case "DEC":
+        case  "DEC" :
             return {
                 ...state,
-                value: state.value - 1
+                value: state.value - 1 
             };
-        case "RND":
+        case  "RND" :
             return {
                 ...state,
-                value: state.value * action.payload
+                value: state.value * action.payload 
             };
-        default:
+        default :
             return state;
+        
     }
+
+    
+
 }
 
+
 const store = createStore(reducer);
+
+
 
 const update = () => {
     document.getElementById('counter').textContent = store.getState().value;
@@ -34,36 +41,39 @@ const update = () => {
 
 store.subscribe(update);
 
-const inc = () => ({type: 'INC'});
-const dec = () => ({type: 'DEC'});
-const rnd = (value) => ({type: 'RND', payload: value});
+const inc = () => ({type: "INC"});
+const dec = () => ({type: "DEC"});
+const rnd = (value) => ({type: "RND", payload: value});
 
 document.getElementById('inc').addEventListener('click', () => {
-    store.dispatch(inc());
+    store.dispatch(inc())
 });
 
 document.getElementById('dec').addEventListener('click', () => {
-    store.dispatch(dec());
-});
+    store.dispatch(dec())
+});  
 
 document.getElementById('rnd').addEventListener('click', () => {
-    const value = Math.floor(Math.random() * 10);
-    store.dispatch(rnd(value));
-});
+    const value =  Math.floor(Math.random() * 10)
+    store.dispatch(rnd(value))
+});  
 
 
+
+
+// console.log(store.getState());
 
 // let state = reducer(initialState, {type: 'INC'});
-// state = reducer(state, {type: 'INC'});
-// state = reducer(state, {type: 'INC'});
-// state = reducer(state, {type: 'INC'});
+//     state = reducer(state, {type: 'INC'});
+//     state = reducer(state, {type: 'INC'});
+//     state = reducer(state, {type: 'INC'});
 // console.log(state);
 
-ReactDOM.render(
+
+
+const root = createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <>
-    
-    </>
-  </React.StrictMode>,
-  document.getElementById('root')
+ 
+  </React.StrictMode>
 );
