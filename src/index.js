cf5e623bde7d77b8ad1eeb63faab535d96d65951
Salavat <salavat2602@mail.ru>
2025-11-18 13:58:57 +0500

    react-redux

--- src/index.js
+++ src/index.js
@@ -1,49 +1,29 @@
 import React, { act } from 'react';
 import { createRoot } from 'react-dom/client';
-import {createStore, bindActionCreators} from 'redux'
+import {createStore} from 'redux'
 import reducer from './reducer';
-import * as actions from './action';
 
+import {Provider} from 'react-redux'
+
+import App from './components/App';
 
 
 const store = createStore(reducer);
 
-const {dispatch, subscribe, getState} = store;
 
-
-
-const update = () => {
-    document.getElementById('counter').textContent = getState().value;
-}
-
-subscribe(update);
-
-// const bindActionCreator = (creator, dispatch) => (...args) => {
-//         dispatch(creator(...args))
-// }
-
-const {inc, dec, rnd} = bindActionCreators(actions, dispatch);
-// const decDispatch = bindActionCreators(dec, dispatch);
-// const rndDispatch = bindActionCreators(rnd, dispatch);
-
-
-
-
-document.getElementById('inc').addEventListener('click',inc);
-
-document.getElementById('dec').addEventListener('click', dec);  
-
-document.getElementById('rnd').addEventListener('click', () => {
-    const value =  Math.floor(Math.random() * 10)
-      rnd(value)
-});  
-
-
-
-
-const root = createRoot(document.getElementById('root'));
+    const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
- 
+       <Provider store={store}>
+         <App/>
+       </Provider>
   </React.StrictMode>
 );
+
+
+
+
+
+
+
+
